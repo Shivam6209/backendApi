07eb90859073aafc199ab123320b522e33a317a7
Shvam Kumar jha <shivammasaistudent@gmail.com>
2023-04-02 12:40:54 +0530

    Update reg.js

--- model/reg.js
+++ model/reg.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 
 const reg_schema = mongoose.Schema({
-    name: { type: String},
-    email: { type: String},
-    password: { type: String },
+    name: { type: String,required:true},
+    email: { type: String,required:true},
+    password: { type: String,required:true},
 
 })
 const reg_model = mongoose.model("signupdetails", reg_schema)
